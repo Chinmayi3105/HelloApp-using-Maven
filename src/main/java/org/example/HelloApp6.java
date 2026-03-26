2797ecd67eef524f2c8c32025f5de963d8dd4620
Chinmayi3105 <vinaychinmayi7@gmail.com>
2026-03-26 09:16:48 +0530

    Added uc6

---
+++ src/main/java/org/example/HelloApp6.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class HelloApp6 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            String finalNames = "";
+            if (!nameBuilder.isEmpty()) {
+                finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+
+            System.out.println("Hello, " + finalNames + "!");
+        }
+    }
+}
